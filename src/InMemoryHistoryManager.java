5f1eaeeab44bcb883529390cdaf440070380c0bc
Di M <itsdianamez@gmail.com>
2025-04-11 16:51:00 +0300

    Техническое задание проекта №7

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -86,21 +86,5 @@ public class InMemoryHistoryManager implements HistoryManager {
 
 }
 
-class Node<T> {
-    public Node<T> next;
-    public Node<T> prev;
-    Task task;
 
-    public Node(Task task) {
-        this.task = task;
-        next = null;
-        prev = null;
-    }
-
-    public Node(Node<T> next, Node<T> prev, Task task) {
-        this.next = next;
-        this.prev = prev;
-        this.task = task;
-    }
-}
 
